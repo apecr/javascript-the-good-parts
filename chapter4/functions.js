32b1fa70993483533ff4801f5e400c3c6bcd3d37
alberto.eyo.contractor <alberto.eyo.contractor@bbva.com>
2017-06-14 18:06:40 +0200

    Modulo 4 hasta closures
    Tests y codigo del tema 4 hasta la seccion de closure

--- chapter4/functions.js
+++ chapter4/functions.js
@@ -2,7 +2,17 @@
 
 var add = function(a, b) {
   return a + b;
-}
+};
+
+var addValidateParams = (a, b) => {
+  if (typeof a !== 'number' || typeof b !== 'number') {
+    throw {
+      name: 'TypeError',
+      message: 'add needs numbers'
+    };
+  }
+  return add(a, b);
+};
 
 var myObject = {
   value: 0,
@@ -21,8 +31,97 @@ myObject.double = function() {
 
 myObject.otherDouble = function() {
   this.value = add(this.value, this.value);
-}
+};
+
+// Create a constructor function called Quo.
+// It makes an object with a status property.
+var Quo = function(string) {
+  this.status = string;
+};
+
+// Give all instances of Quo a public method
+// called get_status.
+Quo.prototype.get_status = function() {
+  return this.status;
+};
+
+
+// Make a function that adds a lot of stuff.
+// Note that defining the variable sum inside of
+// the function does not interfere with the sum
+// defined outside of the function. The function
+// only sees the inner one.
+var sum = function() {
+  var i = 0;
+  var sum = 0;
+  for (i = 0; i < arguments.length; i += 1) {
+    sum += arguments[i];
+  }
+  return sum;
+};
+
+
+Function.prototype.method = function(name, func) {
+  if (!this.prototype[name]) {
+    this.prototype[name] = func;
+  }
+};
+
+Number.method('integer', function() {
+  return Math[this < 0 ? 'ceil' : 'floor'](this);
+});
+String.method('myTrim', function() {
+  return this.replace(/^\s+|\s+$/g, '');
+});
+
+let result = '';
+var hanoi = function(disc, src, aux, dst) {
+  if (disc > 0) {
+    hanoi(disc - 1, src, dst, aux);
+    result += `Move disc ${disc} from ${src} to ${dst} \n`;
+    hanoi(disc - 1, aux, src, dst);
+  }
+  return result;
+};
+
+// Make a factorial function with tail
+// recursion. It is tail recursive because
+// it returns the result of calling itself.
+// JavaScript does not currently optimize this form.
+var factorial = function(i, a) {
+  a = a || 1;
+  if (i < 2) {
+    return a;
+  }
+  return factorial(i - 1, a * i);
+};
+
+var foo = function() {
+  var a = 3;
+  var b = 5;
+  var bar = function() {
+    var b = 7, c = 11;
+
+    // At this point, a is 3, b is 7, and c is 11
+    a += b + c;
+
+    // At this point, a is 21, b is 7, and c is 11
+  };
+
+  // At this point, a is 3, b is 5, and c is not defined
+  bar();
+
+  // At this point, a is 21, b is 5
+  return [a, b];
+};
 
 module.exports = {
-  myObject
+  add,
+  myObject,
+  Quo,
+  sum,
+  addValidateParams,
+  hanoi,
+  factorial,
+  foo
 };
--- test/chapter4/functions.js
+++ test/chapter4/functions.js
@@ -3,7 +3,7 @@
 const functions = require('../../chapter4/functions');
 const expect = require('chai').expect;
 
-/*global define describe, it, afterEach, after*/
+/*global define, describe, it, afterEach, after*/
 describe('Functions module', () => {
   describe('The method invocation pattern', () => {
     it('Increment the value. No parameters', () => {
@@ -45,6 +45,89 @@ describe('Functions module', () => {
 
       //Assert
       expect(object.value).to.equal(6);
+    });
+  });
+  describe('The constructor invocation pattern', () => {
+    it('Get a instnce of Quo', () => {
+      // Act
+      var quo = new functions.Quo('accepted');
+
+      // Assert
+      expect(quo.get_status()).to.equal('accepted');
+    });
+  });
+  describe('The apply invocation pattern', () => {
+    it('Should add with an array for argument (apply invocation)', () => {
+      // Arrange
+      var array = [3, 4];
+
+      // Act
+      var sum = functions.add.apply(null, array);
+
+      // Assert
+      expect(sum).to.equal(7);
+    });
+    it('Calling apply through prototype', () => {
+      // Arrange
+      var statusObject = {
+        status: 'A-OK'
+      };
+
+      // Act
+      var status = functions.Quo.prototype.get_status.apply(statusObject);
+
+      // Assert
+      expect(status).to.equal('A-OK');
+    });
+  });
+  describe('Arguments array in action', () => {
+    it('Should sum all the arguments of the function', () => {
+      expect(functions.sum(1, 2, 3, 4, 5)).to.equal(15);
+    });
+  });
+  describe('Exceptions', () => {
+    it('Calling a function that throw an exception', () => {
+      try {
+        functions.addValidateParams('seven');
+      } catch (e) {
+        expect(e.name).to.equal('TypeError');
+        expect(e.message).to.equal('add needs numbers');
+      }
+    });
+    it('Calling a function that throw an exception. Other way', () => {
+      expect(() => functions.addValidateParams('seven')).to.throw();
+    });
+  });
+  describe('Augment types', () => {
+    it('Using integer from Number becasue method prototype from Function', () => {
+      expect((-10 / 3).integer()).to.equal(-3);
+    });
+    it('Using myTrim function', () => {
+      expect('   hola    '.myTrim()).to.equal('hola');
+    });
+  });
+  const hanoiResult = 'Move disc 1 from Src to Dst \n' +
+    'Move disc 2 from Src to Aux \n' +
+    'Move disc 1 from Dst to Aux \n' +
+    'Move disc 3 from Src to Dst \n' +
+    'Move disc 1 from Aux to Src \n' +
+    'Move disc 2 from Aux to Dst \n' +
+    'Move disc 1 from Src to Dst \n';
+  describe('Recursion', () => {
+    it('Hanoi problem', () => {
+      // Act
+      var resultHanoi = functions.hanoi(3, 'Src', 'Aux', 'Dst');
+
+      // Asser
+      expect(resultHanoi).to.equal(hanoiResult);
+    });
+    it('Factorial', () => {
+      expect(functions.factorial(5)).to.equal(120);
+    });
+  });
+  describe('Scope exmaples', () => {
+    it('Should get the variables from the correct scope', () => {
+      expect(functions.foo()).to.deep.equal([21, 5]);
     });
   });
 });
